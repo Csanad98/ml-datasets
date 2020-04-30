f37dc4b295ecf9e026fe9ebe728c0ce55b02f6d0
c_bakos <csanad@geosignum.com>
2020-04-30 19:23:18 +0200

    improved prep script

--- tsrd_dataset_raw/tsrd_dataset_raw_prep.sh
+++ tsrd_dataset_raw/tsrd_dataset_raw_prep.sh
@@ -2,34 +2,20 @@
 # make a folder for each class and move each corressponding images to the directories
 # this allows to use PyTorch ImageFolder class to easily load the data
 
-#change to train dir
-cd tsrd-train
-for n in {0..9}
+for directory in "tsrd-train" "tsrd-test"
 do
-	mkdir $n
-	mv 00$n* $n
+	cd $directory
+	for n in {0..9}
+	do
+		mkdir $n
+		mv 00$n* $n
+	done
+
+	for n in {10..57}
+	do
+		mkdir $n
+		mv 0$n* $n
+	done
+
+	cd ..
 done
-
-for n in {10..57}
-do
-	mkdir $n
-	mv 0$n* $n
-done
-
-cd ..
-
-#change to test dir
-cd tsrd-test
-for n in {0..9}
-do
-	mkdir $n
-	mv 00$n* $n
-done
-
-for n in {10..57}
-do
-	mkdir $n
-	mv 0$n* $n
-done
-
-cd ..
